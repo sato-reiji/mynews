558784891eac9d2fe28bcfe4e5d3bac170228e17
佐藤玲治 <bikkle1992@gmail.com>
2020-02-26 02:05:44 +0000

    サードコミット

---
+++ resources/views/admin/profile/create.blade.php
@@ -0,0 +1,13 @@
+<!DOCTYPE HTML>
+<html>
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        
+        <title>create</title>
+    </head>
+    <body>
+        <h1>createの方</h1>
+    </body>
+</html>
