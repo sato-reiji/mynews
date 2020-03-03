9aed01d7eb5183cc1115ada80a76e30905d887c7
佐藤玲治 <bikkle1992@gmail.com>
2020-03-03 02:12:32 +0000

    PHP/Laravel 11

--- resources/views/admin/profile/create.blade.php
+++ resources/views/admin/profile/create.blade.php
@@ -1,13 +1,13 @@
-<!DOCTYPE HTML>
-<html>
-    <head>
-        <meta charset="utf-8">
-        <meta http-equiv="X-UA-Compatible" content="IE=edge">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
-        
-        <title>create</title>
-    </head>
-    <body>
-        <h1>createの方</h1>
-    </body>
-</html>
+@extends('layouts.admin')
+
+@section('title','My プロフィール')
+
+@section('content')
+    <div class="container">
+            <div class="row">
+                  <div class="col-md-8 mx-auto">
+                        <h2>My プロフィール</h2>
+                  </div>
+            </div>
+    </div>
+@endsection
