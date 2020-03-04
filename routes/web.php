d129010bcfa5d155ec01e599bee683782ede2cd8
佐藤玲治 <bikkle1992@gmail.com>
2020-03-04 01:43:46 +0000

    PHP/Laravel 13

--- routes/web.php
+++ routes/web.php
@@ -18,6 +18,10 @@ Route::group(['prefix' => 'admin'], function() {
     Route::get('news/create','Admin\NewsController@add')->middleware('auth');
     Route::get('profile/create','Admin\ProfileController@add')->middleware('auth');
     Route::get('profile/edit','Admin\ProfileController@edit')->middleware('auth');
+    Route::post('news/create', 'Admin\NewsController@create');
+    Route::post('profile/create', 'Admin\ProfileController@create');
+    Route::post('profile/edit','Admin\ProfileController@update');
+
 });
 
 Auth::routes();
