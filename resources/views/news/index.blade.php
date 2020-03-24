d400c0d83068c3fcd245d3ad4033391b147ea4d3
佐藤玲治 <bikkle1992@gmail.com>
2020-03-24 14:54:23 +0000

    first commit

--- resources/views/news/index.blade.php
+++ resources/views/news/index.blade.php
@@ -7,37 +7,7 @@
             <div class="row">
                 <div class="headline col-md-10 mx-auto">
                     <div class="row">
-                        <div class="posts col-md-8 mx-auto mt-3">
-                @foreach($posts as $post)
-                    <div class="post">
-                        <div class="row">
-                            <div class="text col-md-6">
-                                <div class="date">
-                                    {{ $post->updated_at->format('Y年m月d日') }}
-                                </div>
-                                <div class="name">
-                                    {{ str_limit($post->name, 150) }}
-                                </div>
-                                <div class="gender">
-                                    {{ str_limit($post->gender, 150) }}
-                                </div>
-                                <div class="hobby">
-                                    {{ str_limit($post->hobby, 150) }}
-                                </div>
-                                <div class="introduction mt-3">
-                                    {{ str_limit($post->introduction, 1500) }}
-                                </div>
-                            </div>
-                            <div class="image col-md-6 text-right mt-4">
-                                @if ($post->image_path)
-                                    <img src="{{ asset('storage/image/' . $post->image_path) }}">
-                                @endif
-                            </div>
-                        </div>
-                    </div>
-                    <hr color="#c0c0c0">
-                @endforeach
-            </div>
+                        
                         <div class="col-md-6">
                             <div class="caption mx-auto">
                                 <div class="image">
